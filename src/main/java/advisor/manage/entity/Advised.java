2ca5afe265f56909eaf521f02038f0573bfb96c8
Lingyun_Huang <huangly@shanghaitech.edu.cn>
2023-07-08 19:23:11 +0800

    Initial commit

---
+++ src/main/java/advisor/manage/entity/Advised.java
@@ -0,0 +1,15 @@
+package advisor.manage.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// lombok notation
+@Data
+@NoArgsConstructor
+public class Advised {
+    // three attributes
+    int id;
+    int sid;
+    int aid;
+
+}
