3b33122f79375a9ca8214502ddf9f9de04409145
Yaqing-Peng <759560274@qq.com>
2023-07-19 23:34:30 +0800

    add javafx

--- src/main/java/advisor/manage/Main.java
+++ src/main/java/advisor/manage/Main.java
@@ -4,6 +4,10 @@ package advisor.manage;
 import advisor.manage.entity.Operation;
 
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import org.apache.ibatis.io.Resources;
 
 import java.io.IOException;
@@ -11,8 +15,25 @@ import java.util.Scanner;
 import java.util.logging.LogManager;
 
 
-public class Main {
+public class Main extends Application {
+
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root,400,400);
+            scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /*public static void main(String[] args) {
         // create an instance of Operation Class
         Operation operation = new Operation();
         // capture exception
@@ -216,7 +237,7 @@ public class Main {
         }catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
 
 }
